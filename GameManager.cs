9a46536b4d5e16a6aac6cdcf78aad6b563e88f2e
illusionimmensi <48955495+illusionimmensi@users.noreply.github.com>
2019-08-10 12:11:33 +0530

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool GameIsOver ;
+
+    public GameObject gameOverUI;
+
+    public GameObject CompleteLevelUI;
+
+    void Start()
+    {
+        GameIsOver = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameIsOver)
+            return;
+
+        if (Input.GetKeyDown("e"))
+        {
+            EndGame();
+        }
+
+        if (PlayerStats.Lives <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame ()
+    {
+        GameIsOver = true;
+        gameOverUI.SetActive(true);
+    }
+
+    public void Winlevel ()
+    {
+        GameIsOver = true;
+        CompleteLevelUI.SetActive(true);
+    }
+}
